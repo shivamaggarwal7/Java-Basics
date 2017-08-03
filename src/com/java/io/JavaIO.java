846289e3c098bc3ae0635f1a514e43ca0102aa63
Aggarwal <556823@cognizant.com>
2017-08-03 15:55:32 +0530

    Append content to an existing file using Filewriter

--- src/com/java/io/JavaIO.java
+++ src/com/java/io/JavaIO.java
@@ -1,7 +1,14 @@
+/*To simply add content to a file,Bufferedwriter can be uses whereas to append
+ * content to an existing file FileWriter needs to be used.
+ * 
+ * In order to buffer the data stream while FileWriter stream is wrapped with BufferedWriter*/
+
 package com.java.io;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -42,10 +49,17 @@ public class JavaIO {
 	public static void writeToFile() {
 		
 		try {
-			BufferedWriter writer=Files.newBufferedWriter(Paths.get(FILE_PATH_WRITE), StandardCharsets.UTF_8);
+			/*BufferedWriter writer=Files.newBufferedWriter(Paths.get(FILE_PATH_WRITE), StandardCharsets.UTF_8);
 			
 			writer.write("Wow");
-			writer.close();
+			writer.close();*/
+			
+			FileWriter fileWriter=new FileWriter(new File(FILE_PATH_WRITE), true);
+			BufferedWriter writer=new BufferedWriter(fileWriter);
+			
+			writer.write("Yayy!!");
+			writer.close();			
+			
 			System.out.println("File writing done!!");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
